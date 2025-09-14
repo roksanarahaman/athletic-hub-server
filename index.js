45bc8185552bff933d969fca89a6e2f238691023
roksanarahaman <roksana.rahaman12@gmail.com>
2025-09-15 01:06:00 +0900

    changes on db

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        await client.connect();
+
 
         const eventsCollection = client.db('eventDB').collection('events');
         const bookedCollection = client.db('eventDB').collection('booking');
@@ -161,8 +161,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
